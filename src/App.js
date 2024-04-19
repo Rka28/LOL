a72efb1a40f1de56f09557ac9cac7e12e9789704
rka28 <karim.bara@epitech.eu>
2024-04-19 16:51:45 +0200

    Fix: errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-
-
-
-// let source = {Image} + Champion.Image.full;
-// let Image = images(`./${someVariable}.png`);
-// let source = (Image = Champion.Image.full)
-
-
 function App() {
   const [Champions, setChampions] = useState();
-  const [images, setImages] = useState();
 
   useEffect(() => {
     fetch('https://ddragon.leagueoflegends.com/cdn/14.7.1/data/fr_FR/champion.json')
@@ -21,10 +12,6 @@ function App() {
   }, []);
 
   if (Champions) {
-    Object.values(Champions).map((champion) => {
-      console.log(champion)
-    })
-
     return (
       <div className="champion-list">
         {Object.values(Champions).map((Champion) => {
@@ -33,9 +20,8 @@ function App() {
             <div className="champion-info" key={Champion.id}>
               <h7>{Champion.title}</h7>
               <h3 className="champion-title">{Champion.name}</h3>
-              <img src={'../champion/' + Champion.image.full}></img>
+              <img src={'../champion/' + Champion.image.full} alt="" />
               <h8 className="champion-title">{Champion.tags[0]}</h8>
-
             </div>
           )
         })}
