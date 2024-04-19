5abf40c822f3988db9d1fa92f58469a1895ae5bc
rka28 <karim.bara@epitech.eu>
2024-04-19 16:23:11 +0200

    Reorganizing folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
+
+
+// let source = {Image} + Champion.Image.full;
+// let Image = images(`./${someVariable}.png`);
+// let source = (Image = Champion.Image.full)
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [Champions, setChampions] = useState();
+  const [images, setImages] = useState();
+
+  useEffect(() => {
+    fetch('https://ddragon.leagueoflegends.com/cdn/14.7.1/data/fr_FR/champion.json')
+      .then(response => response.json())
+      .then(response => setChampions(response.data))
+      .catch(error => console.error('Error fetching Champions:', error));
+  }, []);
+
+  if (Champions) {
+    Object.values(Champions).map((champion) => {
+      console.log(champion)
+    })
+
+    return (
+      <div className="champion-list">
+        {Object.values(Champions).map((Champion) => {
+
+          return (
+            <div className="champion-info" key={Champion.id}>
+              <h7>{Champion.title}</h7>
+              <h3 className="champion-title">{Champion.name}</h3>
+              <img src={'../champion/' + Champion.image.full}></img>
+              <h8 className="champion-title">{Champion.tags[0]}</h8>
+
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+
+  return <div>Loading...</div>;
 }
 
 export default App;
